87e8dc35c04e6d1c06368783fb5067152aff549a
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-10-07 13:25:26 +0300

    Cleanup code. Added new --start_page flag and made the code more modular
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- parser.js
+++ parser.js
@@ -8,7 +8,7 @@ let page_to_search = 1;
 let out_file = 'log.dat';
 let sep = undefined
 let write_headers = false
-let page_num = 1;
+let page_start = 1
 // parse args
 if (argv['type'] != undefined)
     type = argv['type']
@@ -18,11 +18,13 @@ if (argv['out'] != undefined)
     out_file = argv['out']
 if (argv['sep'] != undefined)
     sep = argv['sep']
+if (argv['start_page'] != undefined)
+    start_page = Number(argv['start_page'])
 
 var logStream = fs.createWriteStream(out_file, {flags: 'w'});
 if (argv['write_headers']) {
     logStream.write("Severity" + sep + "CVE" + sep + "Vulnerability" + sep + "Plugin URL" + sep +
-        "Affected Plugin" + sep + "Plugin Version" + sep + "Database" + sep + "Discovery Date\n")
+        "Affected Plugin" + sep + "Plugin Version" + sep + "Database" + sep + "Discovery Date" + sep + "#Page\n")
 }
 
 function write_entry(obj) {
@@ -31,12 +33,12 @@ function write_entry(obj) {
     else 
         logStream.write(obj.severity + sep + obj.vuln_url + sep + obj.type + sep + obj.plugin_url +
             sep + obj.affected_plugin + sep + '"' + obj.plugin_version + '"' +
-            sep + obj.database + sep + '"' + obj.discovery_date + '"\n')
+            sep + obj.database + sep + '"' + obj.discovery_date + '"' + sep + obj.page + '\n')
 }
 
-// start to iterate the pages
-while (page_num <= page_to_search) {
-    let url = 'https://snyk.io/vuln/page/' + page_num++ + '?type=' + type;
+function parse_page(pn, t) {
+    let url = 'https://snyk.io/vuln/page/' + pn + '?type=' + t;
+    // start to iterate the pages
     var req = request('GET', url)
     console.log("Checking:", url)
     const $ = cheerio.load(req.body);
@@ -57,9 +59,19 @@ while (page_num <= page_to_search) {
                 obj.plugin_version = $(fields[1]).find(".semver").text()
                 obj.database = $(fields[2]).text().trim()
                 obj.discovery_date = $(fields[3]).text().trim()
+                obj.page = pn
                 write_entry(obj)
             });
         }
     });
 }
+
+function parse_all_pages(sp, p2s, t) {
+    let page_num = sp;
+    while (page_num < p2s) {
+        parse_page(page_num, t)
+        page_num++;
+    }
+}
+parse_all_pages(start_page, page_to_search + start_page, type)
 console.log("Parsing completed");
